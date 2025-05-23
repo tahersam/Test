781c83eacdd92e8a4bee2f9ed80f1746b484db07
tahersam <tsamerkheda@gmail.com>
2025-05-23 12:27:31 +0530

    second

--- src/main/java/com/jenkins/leaning/LeaningApplication.java
+++ src/main/java/com/jenkins/leaning/LeaningApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeaningApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome from Maven Project!!");
+		System.out.println("Welcome from Maven Project, Have a Nice Day!!");
 		SpringApplication.run(LeaningApplication.class, args);
 	}
 
